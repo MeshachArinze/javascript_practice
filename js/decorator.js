1575953c3e54084e08947cc283a813a0cc54b248
MeshachArinze <meshacharinze@gmail.com>
2023-04-18 11:49:33 +0100

    commit decorator2 to repo

--- js/decorator.js
+++ js/decorator.js
@@ -1,6 +1,6 @@
 // In our case, we can use call in the wrapper to pass the context to the original function
 
-let worker = {
+let works = {
   someMethod() {
     return 1;
   },
@@ -23,7 +23,38 @@ function cachingDecorator(func) {
   };
 }
 
-worker.slow = cachingDecorator(worker.slow); // now make it caching
+works.slow = cachingDecorator(works.slow); // now make it caching
+
+let worker = {
+  slow(min, max) {
+    console.log(`Called with ${min},${max}`);
+    return min + max;
+  }
+};
+
+function cachingDecorator(func, hash) {
+  let cache = new Map();
+  return function() {
+    let key = hash(arguments); // (*)
+    if (cache.has(key)) {
+      return cache.get(key);
+    }
+
+    let result = func.call(this, ...arguments); // (**)
+
+    cache.set(key, result);
+    return result;
+  };
+}
+
+function hash(args) {
+  return args[0] + ',' + args[1];
+}
+
+worker.slow = cachingDecorator(worker.slow, hash);
+
+console.log( worker.slow(3, 5) ); // works
+console.log( "Again " + worker.slow(3, 5) ); // same (cached)
 
 console.log( worker.slow(2) ); // works
 console.log( worker.slow(2) ); // works, doesn't call the original (cached)
